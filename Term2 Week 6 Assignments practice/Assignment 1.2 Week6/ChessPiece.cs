f345ed946b56295dc8765f0852994c9c6813c268
Kingwoods <42969169+MohaymenSameen@users.noreply.github.com>
2020-02-13 20:00:14 +0100

    Add files via upload

---
+++ Term2 Week 6 Assignments practice/Assignment 1.2 Week6/ChessPiece.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1._2_Week6
+{
+    class ChessPiece
+    {
+        public ChessPieceColor Color;
+        public ChessPieceType Type;
+    }
+}
---
+++ Term2 Week 6 Assignments practice/Assignment 1.2 Week6/Enumerations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1._2_Week6
+{
+    enum ChessPieceColor
+    {
+        Black,White
+    }
+    enum ChessPieceType
+    {
+        Pawn,Rook,Knight,Bishop,King,Queen
+    }
+}
